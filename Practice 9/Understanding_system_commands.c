1cc80debfb376228587573b05072c15cdfee7ae5
STROKE-MODS <hs619689@gmail.com>
2025-08-01 20:20:16 +0530

    Add files via upload

---
+++ Practice 9/Understanding_system_commands.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h> //This is main to open a link !!
+int main(){
+        
+    system("open https://www.youtube.com"); //To Open a link use open and system command in c.
+
+    return 0;
+}
